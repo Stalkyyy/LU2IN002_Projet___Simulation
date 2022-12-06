7378745a741ddf2c7b792018aa72a037ba2e7dc0
Stalky <51473069+Stalkyyy@users.noreply.github.com>
2022-12-06 14:33:11 +0100

    Extends Exception class

---
+++ Classes/PasDeRessource.java
@@ -0,0 +1,17 @@
+/*
+    Projet Java - LU2IN002 - 2022
+    PasDeRessource.java
+    
+    PINHO FERNANDES Enzo - 21107465
+    DURBIN Deniz Ali - 21111116
+*/
+
+public class PasDeRessource extends Exception {
+  public PasDeRessource(){
+    super("Pas de ressource ici !"); 
+  }
+  
+  public String toString(){
+    return "PasDeRessource : Il n'y a pas de ressource ici (pas voulu par le fournisseur), il ne se passe rien !"; 
+  }
+}
