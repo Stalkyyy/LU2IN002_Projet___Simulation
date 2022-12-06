aa81d356af0536211f54c92c33ccf3c4b6a9e5e7
Stalky <51473069+Stalkyyy@users.noreply.github.com>
2022-12-06 14:37:44 +0100

    PasDeRessource - Enoncé, extends Exception

--- Classes/PasDeRessource.java
+++ Classes/PasDeRessource.java
@@ -7,6 +7,11 @@
 */
 
 public class PasDeRessource extends Exception {
+  /*
+   * Cette classe permet de renvoyer une exception s'il n'y a pas de ressource dans une position donnée du Terrain.
+   * En théorie, toutes les cases du tableau de Ressources devrait avoir une ressource, s'il n'y en a pas, on renvoit cette exception.
+   */
+    
   public PasDeRessource(){
     super("Pas de ressource ici !"); 
   }
